80f67c899ba18b15444ab5a27664b0e250ceb262
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2022-12-20 12:40:55 +0530

    Submission

--- solution.js
+++ solution.js
@@ -1,7 +1,6 @@
-
-let yourArray;
 // assign your array the correct value in the next line
 // your code here!
+let yourArray = ['thirty', 2007, true, 26, 'love'];
 // code end
 
 module.exports = yourArray
